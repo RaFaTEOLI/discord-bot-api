782ea8e1120c709c89aa2b080c6d7e9c00729fc2
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-11-02 16:56:54 +0100

    test: add mock for cryptography

---
+++ src/data/test/mock-criptography.ts
@@ -0,0 +1,40 @@
+import { Decrypter } from '@/data/protocols/cryptography/decrypter';
+import { Encrypter } from '@/data/protocols/cryptography/encrypter';
+import { HashComparer } from '@/data/protocols/cryptography/hash-comparer';
+import { Hasher } from '@/data/protocols/cryptography/hasher';
+
+export const mockHasher = (): Hasher => {
+  class HasherStub implements Hasher {
+    async hash(value: string): Promise<string> {
+      return await Promise.resolve('hashed_password');
+    }
+  }
+  return new HasherStub();
+};
+
+export const mockDecrypter = (): Decrypter => {
+  class DecrypterStub implements Decrypter {
+    async decrypt(value: string): Promise<string | null> {
+      return await Promise.resolve('any_value');
+    }
+  }
+  return new DecrypterStub();
+};
+
+export const mockHashComparer = (): HashComparer => {
+  class HashComparerStub implements HashComparer {
+    async compare(value: string, hash: string): Promise<boolean> {
+      return await Promise.resolve(true);
+    }
+  }
+  return new HashComparerStub();
+};
+
+export const mockEncrypter = (): Encrypter => {
+  class EncrypterStub implements Encrypter {
+    async encrypt(value: string): Promise<string> {
+      return await Promise.resolve('any_token');
+    }
+  }
+  return new EncrypterStub();
+};
