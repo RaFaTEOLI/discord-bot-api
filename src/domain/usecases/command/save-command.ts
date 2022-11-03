229a9f59b74b54d32505947e3a607fd0803b2fc7
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-11-03 09:43:17 +0100

    feat: add SaveCommand usecase

---
+++ src/domain/usecases/command/save-command.ts
@@ -0,0 +1,8 @@
+import { CommandModel } from '@/domain/models/command';
+
+type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>;
+export type SaveCommandParams = Optional<CommandModel, 'id'>;
+
+export interface SaveCommand {
+  save: (data: SaveCommandParams) => Promise<void>;
+}
