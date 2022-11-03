d3300e13ee1883ff0144074b8160bb2694b13caa
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-11-03 09:34:50 +0100

    test: add unit testing for mongo helpers

---
+++ src/infra/db/mongodb/helpers/mongo-helper.spec.ts
@@ -0,0 +1,14 @@
+import { MongoHelper as sut } from './mongo-helper';
+
+describe('Mongo Helper', () => {
+  beforeAll(async () => await sut.connect(process.env.MONGO_URL ?? ''));
+  afterAll(async () => await sut.disconnect());
+
+  test('should reconnect if mongodb is down', async () => {
+    let accountsCollection = await sut.getCollection('accounts');
+    expect(accountsCollection).toBeTruthy();
+    await sut.disconnect();
+    accountsCollection = await sut.getCollection('accounts');
+    expect(accountsCollection).toBeTruthy();
+  });
+});
