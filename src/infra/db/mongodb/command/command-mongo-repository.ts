ea0b3f0052b367c3484f1bdbedf1117604fa759c
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-11-03 09:43:51 +0100

    feat: ensure CommandMongoRepository will persist and fetch data from mongodb

---
+++ src/infra/db/mongodb/command/command-mongo-repository.ts
@@ -0,0 +1,49 @@
+/* eslint-disable @typescript-eslint/indent */
+/* eslint-disable @typescript-eslint/brace-style */
+
+import { SaveCommandRepository } from '@/data/protocols/db/command/save-command-repository';
+import { SaveCommandParams } from '@/domain/usecases/command/save-command';
+import { LoadCommandByIdRepository } from '@/data/usecases/command/load-command-by-id/db-load-command-by-id-protocols';
+import { CommandModel } from '@/domain/models/command';
+import { LoadCommandsRepository } from '@/data/protocols/db/command/load-commands-repository';
+import { ObjectId } from 'mongodb';
+import { MongoHelper } from '../helpers/mongo-helper';
+
+export class CommandMongoRepository
+  implements SaveCommandRepository, LoadCommandsRepository, LoadCommandByIdRepository
+{
+  async save(commandData: SaveCommandParams): Promise<void> {
+    const commandCollection = await MongoHelper.getCollection('commands');
+    await commandCollection.findOneAndUpdate(
+      {
+        _id: new ObjectId(commandData.id)
+      },
+      {
+        $set: {
+          command: commandData.command,
+          dispatcher: commandData.dispatcher,
+          type: commandData.type,
+          description: commandData.description,
+          response: commandData.response,
+          message: commandData.message
+        }
+      },
+      {
+        upsert: true,
+        returnDocument: 'after'
+      }
+    );
+  }
+
+  async loadAll(): Promise<CommandModel[]> {
+    const commandCollection = await MongoHelper.getCollection('commands');
+    const commands = await commandCollection.find().toArray();
+    return MongoHelper.formatArray(commands);
+  }
+
+  async loadById(id: string): Promise<CommandModel> {
+    const commandCollection = await MongoHelper.getCollection('commands');
+    const command = await commandCollection.findOne({ _id: new ObjectId(id) });
+    return MongoHelper.format(command);
+  }
+}
