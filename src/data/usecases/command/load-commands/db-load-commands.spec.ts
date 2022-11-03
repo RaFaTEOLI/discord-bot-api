0e4ac9cd0bf74e8123ad1b8b3299fab01ca67ced
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-11-03 09:41:32 +0100

    test: ensure DbLoadCommands will return all commands

---
+++ src/data/usecases/command/load-commands/db-load-commands.spec.ts
@@ -0,0 +1,49 @@
+import { LoadCommandsRepository } from './db-load-commands-protocols';
+import { DbLoadCommands } from './db-load-commands';
+import MockDate from 'mockdate';
+import { mockLoadCommandsRepository } from '@/data/test';
+import { mockCommandsData } from '@/domain/test';
+
+interface SutTypes {
+  sut: DbLoadCommands;
+  loadCommandsRepositoryStub: LoadCommandsRepository;
+}
+
+const makeSut = (): SutTypes => {
+  const loadCommandsRepositoryStub = mockLoadCommandsRepository();
+  const sut = new DbLoadCommands(loadCommandsRepositoryStub);
+  return {
+    sut,
+    loadCommandsRepositoryStub
+  };
+};
+
+describe('DbLoadCommands', () => {
+  beforeAll(() => {
+    MockDate.set(new Date());
+  });
+
+  afterAll(() => {
+    MockDate.reset();
+  });
+
+  test('should call LoadCommandsRepository', async () => {
+    const { sut, loadCommandsRepositoryStub } = makeSut();
+    const loadAllSpy = jest.spyOn(loadCommandsRepositoryStub, 'loadAll');
+    await sut.load();
+    expect(loadAllSpy).toHaveBeenCalled();
+  });
+
+  test('should return a list of Commands on success', async () => {
+    const { sut } = makeSut();
+    const commands = await sut.load();
+    expect(commands).toEqual(mockCommandsData());
+  });
+
+  test('should throw exception if LoadCommandsRepository throws exception', async () => {
+    const { sut, loadCommandsRepositoryStub } = makeSut();
+    jest.spyOn(loadCommandsRepositoryStub, 'loadAll').mockRejectedValueOnce(new Error());
+    const promise = sut.load();
+    await expect(promise).rejects.toThrow();
+  });
+});
