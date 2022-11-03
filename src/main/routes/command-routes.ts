ee72f071aa54a661ff44237e420e880c4b05fd17
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-11-03 12:35:59 +0100

    feat: add command routes

---
+++ src/main/routes/command-routes.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import { adaptRoute } from '@/main/adapters/express-route-adapter';
+import { makeSaveCommandController } from '@/main/factories/controllers/command/save-command/save-command-controller-factory';
+import { makeLoadCommandsController } from '@/main/factories/controllers/command/load-commands/load-commands-controller-factory';
+import { makeUpdateCommandController } from '@/main/factories/controllers/command/update-command/update-command-controller-factory';
+import { makeLoadCommandByIdController } from '@/main/factories/controllers/command/load-command-by-id/load-command-by-id-controller-factory';
+import { adminAuth } from '@/main/middlewares/auth/admin-auth';
+import { auth } from '@/main/middlewares/auth/auth';
+
+export default (router: Router): void => {
+  router.post('/commands', adminAuth, adaptRoute(makeSaveCommandController()));
+  router.put('/commands/:commandId', adminAuth, adaptRoute(makeUpdateCommandController()));
+  router.get('/commands', auth, adaptRoute(makeLoadCommandsController()));
+  router.get('/commands/:commandId', auth, adaptRoute(makeLoadCommandByIdController()));
+};
