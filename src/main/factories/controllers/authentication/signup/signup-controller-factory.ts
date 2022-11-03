e3986ae95a3f894fdef8bf1c411944abb7745af8
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-11-03 09:32:28 +0100

    feat: add signup composition

---
+++ src/main/factories/controllers/authentication/signup/signup-controller-factory.ts
@@ -0,0 +1,16 @@
+import { SignUpController } from '@/presentation/controllers/authentication/signup/signup-controller';
+import { Controller } from '@/presentation/protocols';
+import { makeSignUpValidation } from './signup-validation-factory';
+import { makeDbAuthentication } from '@/main/factories/usecases/account/authentication/db-authentication-factory';
+import { makeDbAddAccount } from '@/main/factories/usecases/account/add-account/db-add-account-factory';
+import { makeLogControllerDecorator } from '@/main/factories/decorators/log-controller-decorator-factory';
+
+export const makeSignUpController = (): Controller => {
+  return makeLogControllerDecorator(
+    new SignUpController(
+      makeDbAddAccount(),
+      makeSignUpValidation(),
+      makeDbAuthentication()
+    )
+  );
+};
