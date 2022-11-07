d1266711549e5130bdf07e84600fb440473b10fc
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-11-07 11:55:03 +0100

    test: ensure DbLoadQueue will load queue from LoadQueueRepository

--- src/data/test/mock-db-queue.ts
+++ src/data/test/mock-db-queue.ts
@@ -1,5 +1,8 @@
 import { SaveQueueRepository } from '@/data/protocols/db/queue/save-queue-repository';
+import { LoadQueueRepository } from '@/data/protocols/db/queue/load-queue-repository';
+import { mockQueueModel } from '@/domain/test';
 import { SaveQueueParams } from '@/domain/usecases/queue/save-queue';
+import { QueueModel } from '@/domain/models/queue';
 
 export const mockSaveQueueRepository = (): SaveQueueRepository => {
   class SaveQueueRepositoryStub implements SaveQueueRepository {
@@ -8,4 +11,13 @@ export const mockSaveQueueRepository = (): SaveQueueRepository => {
     }
   }
   return new SaveQueueRepositoryStub();
+};
+
+export const mockLoadQueueRepository = (fakeQueue = mockQueueModel()): LoadQueueRepository => {
+  class LoadQueueRepositoryStub implements LoadQueueRepository {
+    async load(): Promise<QueueModel> {
+      return await Promise.resolve(fakeQueue);
+    }
+  }
+  return new LoadQueueRepositoryStub();
 };
---
+++ src/data/usecases/queue/load-queue/db-load-queue.spec.ts
@@ -0,0 +1,36 @@
+import { LoadQueueRepository, QueueModel } from './db-load-queue-protocols';
+import { DbLoadQueue } from './db-load-queue';
+import { mockLoadQueueRepository } from '@/data/test';
+import { mockQueueModel } from '@/domain/test';
+
+interface SutTypes {
+  sut: DbLoadQueue;
+  loadQueueRepositoryStub: LoadQueueRepository;
+  fakeQueue: QueueModel;
+}
+
+const makeSut = (): SutTypes => {
+  const fakeQueue = mockQueueModel(5);
+  const loadQueueRepositoryStub = mockLoadQueueRepository(fakeQueue);
+  const sut = new DbLoadQueue(loadQueueRepositoryStub);
+  return {
+    sut,
+    loadQueueRepositoryStub,
+    fakeQueue
+  };
+};
+
+describe('DbLoadQueue Usecase', () => {
+  test('should call LoadQueueRepository', async () => {
+    const { sut, loadQueueRepositoryStub } = makeSut();
+    const loadSpy = jest.spyOn(loadQueueRepositoryStub, 'load');
+    await sut.load();
+    expect(loadSpy).toHaveBeenCalled();
+  });
+
+  test('should return a queue on LoadQueueRepository success', async () => {
+    const { sut, fakeQueue } = makeSut();
+    const queue = await sut.load();
+    expect(queue.length).toBe(fakeQueue.length);
+  });
+});
