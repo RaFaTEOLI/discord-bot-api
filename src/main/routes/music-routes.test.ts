ad00b331b39bef9d1341b1bb880562571cf36ad3
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-11-07 09:43:26 +0100

    test: ensure Music Routes will call LoadMusicController correctly

--- src/main/routes/music-routes.test.ts
+++ src/main/routes/music-routes.test.ts
@@ -5,16 +5,17 @@ import app from '@/main/config/app';
 import { sign } from 'jsonwebtoken';
 import env from '@/main/config/env';
 import { mockMusicModel } from '@/domain/test';
+import { faker } from '@faker-js/faker';
 
 let musicCollection: Collection;
 let accountCollection: Collection;
 
-const makeAccessToken = async (): Promise<string> => {
+const makeAccessToken = async (admin = true): Promise<string> => {
   const result = await accountCollection.insertOne({
     name: 'Rafael',
     email: 'rafinha.tessarolo@hotmail.com',
     password: '1234',
-    role: 'admin'
+    ...(admin && { role: 'admin' })
   });
 
   const id = result.insertedId.toString();
@@ -56,6 +57,39 @@ describe('music Routes', () => {
     test('should return 204 on music creation with a valid admin accessToken', async () => {
       const accessToken = await makeAccessToken();
       await request(app).post('/api/music').set('x-access-token', accessToken).send(mockMusicModel()).expect(204);
+    });
+  });
+
+  describe('GET /music', () => {
+    test('should return 403 on load music without accessToken', async () => {
+      await request(app).get('/api/music').expect(403);
+    });
+
+    test('should return 200 on load music with valid admin accessToken', async () => {
+      const accessToken = await makeAccessToken();
+
+      await musicCollection.insertOne({
+        name: faker.random.word(),
+        startedAt: Math.floor(Date.now() / 1000)
+      });
+
+      await request(app).get('/api/music').set('x-access-token', accessToken).expect(200);
+    });
+
+    test('should return 200 on load music with valid normal accessToken', async () => {
+      const accessToken = await makeAccessToken(false);
+
+      await musicCollection.insertOne({
+        name: faker.random.word(),
+        startedAt: Math.floor(Date.now() / 1000)
+      });
+
+      await request(app).get('/api/music').set('x-access-token', accessToken).expect(200);
+    });
+
+    test('should return 200 on load empty music with valid accessToken', async () => {
+      const accessToken = await makeAccessToken();
+      await request(app).get('/api/music').set('x-access-token', accessToken).expect(200);
     });
   });
 });
