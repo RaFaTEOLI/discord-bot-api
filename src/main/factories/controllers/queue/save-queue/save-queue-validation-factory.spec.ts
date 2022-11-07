0933f591c4f20a3cbd9d4ff471128f41d44f5760
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-11-07 11:09:24 +0100

    feat: add save-queue composition

---
+++ src/main/factories/controllers/queue/save-queue/save-queue-validation-factory.spec.ts
@@ -0,0 +1,16 @@
+import { RequiredFieldValidation, ValidationComposite } from '@/validation/validators';
+import { Validation } from '@/presentation/protocols/validation';
+import { makeSaveQueueValidation } from './save-queue-validation-factory';
+
+jest.mock('@/validation/validators/validation-composite');
+
+describe('SaveMusic Validation Factory', () => {
+  test('should call ValidationComposite with all validations', () => {
+    makeSaveQueueValidation();
+    const validations: Validation[] = [];
+    for (const field of ['songs']) {
+      validations.push(new RequiredFieldValidation(field));
+    }
+    expect(ValidationComposite).toHaveBeenCalledWith(validations);
+  });
+});
