f59d7f0e983fab1bdb50f7c5eb7a4d063a8c118d
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-11-07 12:04:47 +0100

    feat: ensure LoadQueueController will call LoadQueue and return queue correctly

---
+++ src/presentation/controllers/queue/load-queue/load-queue-controller.ts
@@ -0,0 +1,18 @@
+import { noContent, serverError, success } from '@/presentation/helpers/http/http-helper';
+import { Controller, HttpResponse, LoadQueue } from './load-queue-controller-protocols';
+
+export class LoadQueueController implements Controller {
+  constructor(private readonly loadQueue: LoadQueue) {}
+
+  async handle(): Promise<HttpResponse> {
+    try {
+      const queue = await this.loadQueue.load();
+      if (!queue?.length) {
+        return noContent();
+      }
+      return success(queue);
+    } catch (error) {
+      return serverError(error);
+    }
+  }
+}
