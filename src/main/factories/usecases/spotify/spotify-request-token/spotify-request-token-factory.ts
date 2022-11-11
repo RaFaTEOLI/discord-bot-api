0034b43943a30db6a583428f8385da858d538514
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-11-11 22:02:06 +0100

    refactor: remove unnecessary dependencies from spotify factory

--- src/main/factories/usecases/spotify/spotify-request-token/spotify-request-token-factory.ts
+++ src/main/factories/usecases/spotify/spotify-request-token/spotify-request-token-factory.ts
@@ -1,15 +1,7 @@
-import { RemoteSpotifyRequestToken } from '@/data/usecases/spotify-request-token/remote-spotify-request-token';
+import { RemoteSpotifyRequestToken } from '@/data/usecases/spotify/request-token/remote-spotify-request-token';
 import { SpotifyRequestToken } from '@/domain/usecases/spotify/spotify-request-token';
-import { AccountMongoRepository } from '@/infra/db/mongodb/account/account-mongo-repository';
 import { makeAxiosHttpClient } from '@/main/factories/http';
 
 export const makeSpotifyRequestToken = (): SpotifyRequestToken => {
-  const accountMongoRepository = new AccountMongoRepository();
-  return new RemoteSpotifyRequestToken(
-    'https://accounts.spotify.com/api/token',
-    makeAxiosHttpClient(),
-    accountMongoRepository,
-    makeAxiosHttpClient(),
-    accountMongoRepository
-  );
+  return new RemoteSpotifyRequestToken('https://accounts.spotify.com/api/token', makeAxiosHttpClient());
 };
