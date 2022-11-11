3d38cc6f7423550dcedc95ca8f3c1c38917fbd90
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-11-11 19:43:28 +0100

    feat: add SpotifyAuthenticateController protocols

---
+++ src/presentation/controllers/spotify/spotify-authenticate/spotify-authenticate-controller-protocols.ts
@@ -0,0 +1,5 @@
+export * from '@/presentation/protocols';
+export * from '@/domain/usecases/spotify/spotify-request-token';
+export * from '@/domain/usecases/account/add-account';
+export * from '@/domain/usecases/account/authentication';
+export * from '@/domain/usecases/spotify/spotify-load-user';
