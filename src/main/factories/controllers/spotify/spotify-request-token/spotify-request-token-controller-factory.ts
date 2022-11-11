3190a6c8ad524921c26ed23c972afbe6bb5bbf23
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-11-11 19:44:26 +0100

    refactor: rename spotify request token to authenticate controller

--- src/main/factories/controllers/spotify/spotify-request-token/spotify-request-token-controller-factory.ts
+++
@@ -1,8 +0,0 @@
-import { Controller } from '@/presentation/protocols';
-import { makeLogControllerDecorator } from '@/main/factories/decorators/log-controller-decorator-factory';
-import { makeSpotifyRequestToken } from '@/main/factories/usecases/spotify/spotify-request-token/spotify-request-token-factory';
-import { SpotifyRequestTokenController } from '@/presentation/controllers/spotify/spotify-request-token/spotify-request-token-controller';
-
-export const makeSpotifyRequestTokenController = (): Controller => {
-  return makeLogControllerDecorator(new SpotifyRequestTokenController(makeSpotifyRequestToken()));
-};
