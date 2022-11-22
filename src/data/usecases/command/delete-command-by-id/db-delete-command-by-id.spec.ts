2468c89962f53575b347a73a53a4a4522b213dd9
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-11-22 08:44:59 +0100

    test: ensure DbDeleteCommandById will call DeleteCommandByIdRepository correctly

---
+++ src/data/usecases/command/delete-command-by-id/db-delete-command-by-id.spec.ts
@@ -0,0 +1,41 @@
+import { DeleteCommandByIdRepository } from './db-delete-command-by-id-protocols';
+import { DbDeleteCommandById } from './db-delete-command-by-id';
+import { mockDeleteCommandByIdRepository } from '@/data/test';
+import { faker } from '@faker-js/faker';
+
+interface SutTypes {
+  sut: DbDeleteCommandById;
+  deleteCommandByIdRepositoryStub: DeleteCommandByIdRepository;
+}
+
+const makeSut = (): SutTypes => {
+  const deleteCommandByIdRepositoryStub = mockDeleteCommandByIdRepository();
+  const sut = new DbDeleteCommandById(deleteCommandByIdRepositoryStub);
+  return {
+    sut,
+    deleteCommandByIdRepositoryStub
+  };
+};
+
+describe('DbDeleteCommandById', () => {
+  test('should call DeleteCommandByIdRepository with correct id', async () => {
+    const { sut, deleteCommandByIdRepositoryStub } = makeSut();
+    const deleteByIdSpy = jest.spyOn(deleteCommandByIdRepositoryStub, 'deleteById');
+    const id = faker.datatype.uuid();
+    await sut.deleteById(id);
+    expect(deleteByIdSpy).toHaveBeenCalledWith(id);
+  });
+
+  test('should return void on success', async () => {
+    const { sut } = makeSut();
+    const command = await sut.deleteById(faker.datatype.uuid());
+    expect(command).toBeFalsy();
+  });
+
+  test('should throw exception if DeleteCommandByIdRepository throws exception', async () => {
+    const { sut, deleteCommandByIdRepositoryStub } = makeSut();
+    jest.spyOn(deleteCommandByIdRepositoryStub, 'deleteById').mockRejectedValueOnce(new Error());
+    const promise = sut.deleteById(faker.datatype.uuid());
+    await expect(promise).rejects.toThrow();
+  });
+});
