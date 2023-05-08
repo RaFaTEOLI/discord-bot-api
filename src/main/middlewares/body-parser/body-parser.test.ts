99b61ab25cbe667d212433b14d2e7558a6a39a97
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2023-05-08 10:33:24 -0300

    refactor: replace import with express app

--- src/main/middlewares/body-parser/body-parser.test.ts
+++ src/main/middlewares/body-parser/body-parser.test.ts
@@ -1,14 +1,11 @@
 import request from 'supertest';
-import app from '@/main/config/app';
+import { expressApp as app } from '@/main/config/app';
 
 describe('Body Parser Middleware', () => {
   test('should parse body as json', async () => {
     app.post('/test-body-parser', (req, res) => {
       res.send(req.body);
     });
-    await request(app)
-      .post('/test-body-parser')
-      .send({ name: 'Rafael' })
-      .expect({ name: 'Rafael' });
+    await request(app).post('/test-body-parser').send({ name: 'Rafael' }).expect({ name: 'Rafael' });
   });
 });
