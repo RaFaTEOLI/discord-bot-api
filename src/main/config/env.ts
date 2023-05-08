73bcc499b366258ddad022baab67e4ceb9b3c937
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2023-05-08 09:29:55 -0300

    chore: setup socket server

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,5 +2,6 @@ export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://root:1234@localhost:27017/',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || '82r628f72998k',
-  commandPrefix: '!'
+  commandPrefix: '!',
+  appUrl: 'http://localhost:5173'
 };
