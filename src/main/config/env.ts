9a58a0ad60dbe0974dbb6e7e3efc1a03c42cd65a
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2023-05-15 08:39:49 -0300

    chore: update env vars and add example

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -3,5 +3,5 @@ export default {
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || '82r628f72998k',
   commandPrefix: '!',
-  appUrl: 'http://localhost:5173'
+  appUrl: process.env.APP_URL || 'http://localhost:5173'
 };
