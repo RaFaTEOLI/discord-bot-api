80dc56f7b2e39b7cee43e5a080ef409d10f93083
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2023-05-29 10:58:59 -0300

    refactor: remove role and accessToken from SaveAccountParams

--- src/domain/usecases/account/save-account.ts
+++ src/domain/usecases/account/save-account.ts
@@ -1,12 +1,14 @@
 import { AccountCleanModel, AccountModel } from '@/domain/models/account';
 
+type AccountCleaned = Omit<AccountCleanModel, 'accessToken' | 'role'>;
+
 type Partial<T> = {
   [P in keyof T]?: T[P];
 };
 
 type WithRequired<T, K extends keyof T> = T & { [P in K]-?: T[P] };
 
-export type SaveAccountParams = WithRequired<Partial<AccountCleanModel>, 'id'>;
+export type SaveAccountParams = WithRequired<Partial<AccountCleaned>, 'id'>;
 
 export interface SaveAccount {
   save: (data: SaveAccountParams) => Promise<AccountModel>;
