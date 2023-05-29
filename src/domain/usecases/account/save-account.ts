4832a654970e46c6bde5a6fc95cd9c4e55374b0c
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2023-05-29 10:52:50 -0300

    refactor: replace AccountModel with AccountCleanModel

--- src/domain/usecases/account/save-account.ts
+++ src/domain/usecases/account/save-account.ts
@@ -1,4 +1,4 @@
-import { AccountModel } from '@/domain/models/account';
+import { AccountCleanModel, AccountModel } from '@/domain/models/account';
 
 type Partial<T> = {
   [P in keyof T]?: T[P];
@@ -6,7 +6,7 @@ type Partial<T> = {
 
 type WithRequired<T, K extends keyof T> = T & { [P in K]-?: T[P] };
 
-export type SaveAccountParams = WithRequired<Partial<AccountModel>, 'id'>;
+export type SaveAccountParams = WithRequired<Partial<AccountCleanModel>, 'id'>;
 
 export interface SaveAccount {
   save: (data: SaveAccountParams) => Promise<AccountModel>;
