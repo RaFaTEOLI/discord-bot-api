00fecb278cf43681a1b1be64f943966b2a05ec6b
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2023-06-13 14:45:56 -0300

    feat: add spotify refresh token route

--- src/main/routes/spotify-routes.ts
+++ src/main/routes/spotify-routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import { adaptRoute } from '@/main/adapters/express-route-adapter';
 import { makeSpotifyAuthenticateController } from '@/main/factories/controllers/spotify/spotify-authenticate/spotify-authenticate-controller-factory';
+import { makeSpotifyRefreshTokenController } from '@/main/factories/controllers/spotify/spotify-refresh-token/spotify-refresh-token-controller-factory';
 
 export default (router: Router): void => {
   router.post('/spotify/auth', adaptRoute(makeSpotifyAuthenticateController()));
+  router.post('/spotify/refresh-token', adaptRoute(makeSpotifyRefreshTokenController()));
 };
