f489e3d853827fb2464e462e8da71875a359f1fb
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2023-10-31 09:19:06 -0300

    fix: adjust export for save-command protocols

--- src/data/usecases/command/save-command/db-save-command-protocols.ts
+++ src/data/usecases/command/save-command/db-save-command-protocols.ts
@@ -1,4 +1,4 @@
 export * from '@/domain/usecases/command/save-command';
 export * from '@/domain/models/command';
-export * from '@/data/protocols/db/command/save-command-repository';
+export { SaveCommandRepository } from '@/data/protocols/db/command/save-command-repository';
 export * from '@/data/protocols/db/command/load-command-by-name-repository';
