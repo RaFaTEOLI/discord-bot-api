5002aa21b22df5c98672af68142957a0f69860e4
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2024-01-31 13:06:43 -0300

    feat: create integration test helper

---
+++ src/main/config/integration-test-helper.ts
@@ -0,0 +1,48 @@
+import { unlink, readFile } from 'fs';
+import path from 'path';
+
+const filePath = path.join(__dirname, 'loaded_routes.json');
+
+export const cleanUpLoadedRoutes = async (): Promise<void> => {
+  return await new Promise((resolve, reject) => {
+    unlink(filePath, err => {
+      if (err) {
+        console.error(`Error deleting loaded_routes.json: ${err.message}`);
+        reject(err);
+      }
+      resolve();
+    });
+  });
+};
+
+export const loadRoutes = async (): Promise<string[]> => {
+  return await new Promise((resolve, reject) => {
+    readFile(filePath, 'utf8', (err, data) => {
+      if (err) reject(err);
+      const parsedData = JSON.parse(data ?? '[]');
+      resolve(parsedData);
+    });
+  });
+};
+
+export const isRouteLoaded = async (route: string): Promise<boolean> => {
+  try {
+    const loadedRoutes = await loadRoutes();
+    return loadedRoutes.includes(route);
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
+};
+
+export const waitForRouteToLoad = async (route: string): Promise<void> => {
+  return await new Promise(resolve => {
+    const interval = setInterval(async () => {
+      const loaded = await isRouteLoaded(route);
+      if (loaded) {
+        clearInterval(interval);
+        resolve();
+      }
+    }, 100);
+  });
+};
