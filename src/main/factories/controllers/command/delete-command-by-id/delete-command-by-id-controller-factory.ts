9b28cc4c607c0e50004dc40e4972e6a979941959
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2024-02-02 17:29:53 -0300

    refactor: add new factories to DeleteCommandByIdController

--- src/main/factories/controllers/command/delete-command-by-id/delete-command-by-id-controller-factory.ts
+++ src/main/factories/controllers/command/delete-command-by-id/delete-command-by-id-controller-factory.ts
@@ -2,7 +2,16 @@ import { Controller } from '@/presentation/protocols';
 import { makeLogControllerDecorator } from '@/main/factories/decorators/log-controller-decorator-factory';
 import { makeDbDeleteCommandById } from '@/main/factories/usecases/command/delete-command-by-id/db-delete-command-by-id-factory';
 import { DeleteCommandByIdController } from '@/presentation/controllers/command/delete-command-by-id/delete-command-by-id-controller';
+import { makeDbLoadCommandById } from '@/main/factories/usecases/command/load-command-by-id/db-load-command-by-id-factory';
+import { makeAmqpClient, useApiQueueFactory } from '@/main/factories/queue';
 
 export const makeDeleteCommandByIdController = (): Controller => {
-  return makeLogControllerDecorator(new DeleteCommandByIdController(makeDbDeleteCommandById()));
+  return makeLogControllerDecorator(
+    new DeleteCommandByIdController(
+      makeDbLoadCommandById(),
+      makeDbDeleteCommandById(),
+      makeAmqpClient(),
+      useApiQueueFactory()
+    )
+  );
 };
