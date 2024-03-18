9b7b1cacd3773202bf746b10533fcd708427588e
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2024-03-18 17:38:41 -0300

    feat: ensure RemoteLoadDiscordCommands will call discord api correctly

---
+++ src/data/usecases/command/load-discord-commands/remote-load-discord-commands.ts
@@ -0,0 +1,26 @@
+import { HttpClient, HttpStatusCode } from '@/data/protocols/http';
+import { AccessDeniedError, UnexpectedError } from '@/domain/errors';
+import { DiscordCommandModel } from '@/domain/models/discord-command-model';
+import { LoadDiscordCommands } from '@/domain/usecases/command/load-discord-commands';
+
+export class RemoteLoadDiscordCommands implements LoadDiscordCommands {
+  constructor(private readonly url: string, private readonly httpClient: HttpClient<DiscordCommandModel[]>) {}
+
+  async all(): Promise<DiscordCommandModel[]> {
+    const httpResponse = await this.httpClient.request({
+      url: this.url,
+      method: 'get'
+    });
+    const remoteCommands = httpResponse.body ?? [];
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.success:
+        return remoteCommands;
+      case HttpStatusCode.noContent:
+        return [];
+      case HttpStatusCode.forbidden:
+        throw new AccessDeniedError();
+      default:
+        throw new UnexpectedError();
+    }
+  }
+}
